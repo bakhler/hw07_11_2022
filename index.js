a7eae3eb97c3d55cc0d4cc0e711ab85bf6136dcd
bakhler <thehenridit@gmail.com>
2022-11-08 21:35:56 +0200

    Did arephmatic with 45;12; and used the assignment operator.

---
+++ index.js
@@ -0,0 +1,17 @@
+let number1 = 45;
+let number2 = 12;
+
+console.log(number1+number2)
+console.log(number1-number2)
+console.log(number1/number2)
+console.log(number1*number2)
+console.log(number1%number2)
+
+let number3 = 456;
+number3 +=6;
+number3++;
+number3/=2;
+number3-=3;
+number3--;
+number3+='qwerty';
+
